c9fae3f8b7804040628878d1d140f28aaed9f473
haswelltitan <545254534@qq.com>
2018-04-10 21:44:10 +0800

    k-fold on train set

---
+++ cross_validation.py
@@ -0,0 +1,26 @@
+from time import time
+import tensorflow
+start = time()
+
+# load train data
+positive = []
+negative = []
+train_label = []
+count = 0
+for buffer in open('train.csv').readlines()[1:]:
+    flag = int(buffer[-2])
+    train_label.append(flag)
+    if flag == 1:
+        positive.append(count)
+    else:
+        negative.append(count)
+    count += 1
+train = []
+for vector in open('train.txt').readlines()[:100000]:
+    temp = []
+    for element in vector.split():
+        temp.append(int(element))
+    train.append(temp)
+end = time()
+print(end-start, 's')
+print('\n')
