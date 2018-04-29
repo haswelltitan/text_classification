dc030e6dfe86ae683c2ee7389a8ddd596abae0b3
haswelltitan <545254534@qq.com>
2018-04-29 20:15:34 +0800

    return to word segmentation

--- read.py
+++ read.py
@@ -3,33 +3,28 @@ from time import time
 import jieba.analyse
 start = time()
 
-min_sup = 20
-topK = 10
-train = []
-dic = {}
 jieba.enable_parallel(4)
-
-# exclude numbers & irrelevant characters
+dic = {}
+train = []
 for line in open('train.json').readlines():
     text = ""
     for char in loads(line)['content']:
         if 19968 <= ord(char) <= 40869:
             text += char
-    key_words = list(jieba.analyse.extract_tags(text, topK))
+    key_words = list(jieba.analyse.extract_tags(text))
     train.append(key_words)
     for word in key_words:
         if word in dic:
             dic[word] += 1
         else:
             dic[word] = 1
-
 test = []
 for line in open('test.json').readlines():
     text = ""
     for char in loads(line)['content']:
         if 19968 <= ord(char) <= 40869:
             text += char
-    key_words = list(jieba.analyse.extract_tags(text, topK))
+    key_words = list(jieba.analyse.extract_tags(text))
     test.append(key_words)
     for word in key_words:
         if word in dic:
@@ -38,28 +33,22 @@ for line in open('test.json').readlines():
             dic[word] = 1
 jieba.disable_parallel()
 
-# find set of frequent words
-top_words = sorted(dic.keys(), key=dic.__getitem__, reverse=True)[:1000]
-
-# extract features for train and test data
+top_words = sorted(dic.keys(), key=dic.__getitem__, reverse=True)[:256]
 file = open('train.txt', 'w')
 for text in train:
-    for word in top_words:
-        file.write(str(text.count(word))+' ')
+    for pos in range(len(top_words)):
+        if top_words[pos] in text:
+            file.write(str(pos)+' ')
     file.write('\n')
 file.close()
-
 file = open('test.txt', 'w')
 for text in test:
-    for word in top_words:
-        file.write(str(text.count(word))+' ')
+    for pos in range(len(top_words)):
+        if top_words[pos] in text:
+            file.write(str(pos)+' ')
     file.write('\n')
 file.close()
 
-# train_label = []
-# for buffer in open('train.csv').readlines()[1:]:
-#     train_label.append(int(buffer[-2]))
-
 end = time()
-print(end - start, 's')
+print(end-start, 's')
 print('\n')
