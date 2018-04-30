c45c9959b79b0e71e6a7362761a738e632198102
haswelltitan <545254534@qq.com>
2018-04-30 17:43:12 +0800

    stop words instead of tfidf

--- segmentation.py
+++ segmentation.py
@@ -7,6 +7,7 @@ stop_words = []
 for line in open('stop_words.txt', encoding='utf_8').readlines():
     stop_words.append(line.strip())
 
+num = 0
 outfile = open('train_seg.txt', 'w')
 for buffer in open('train.json', encoding='utf-8').readlines():
     line = ''
@@ -14,10 +15,13 @@ for buffer in open('train.json', encoding='utf-8').readlines():
         if 19968 <= ord(char) <= 40869:
             line += char
     raw = list(jieba.cut(line, cut_all=False))
-    for word in stop_words:
-        if word in raw:
-            raw.remove(word)
-    outfile.write(' '.join(raw)+'\n')
+    temp = []
+    for word in raw:
+        if word not in stop_words:
+            temp.append(word)
+    outfile.write(' '.join(temp)+'\n')
+    num += 1
+    print(num)
 outfile.close()
 
 outfile = open('test_seg.txt', 'w')
@@ -27,10 +31,11 @@ for buffer in open('test.json', encoding='utf-8').readlines():
         if 19968 <= ord(char) <= 40869:
             line += char
     raw = list(jieba.cut(line, cut_all=False))
-    for word in stop_words:
-        if word in raw:
-            raw.remove(word)
-    outfile.write(' '.join(raw) + '\n')
+    temp = []
+    for word in raw:
+        if word not in stop_words:
+            temp.append(word)
+    outfile.write(' '.join(temp)+'\n')
 outfile.close()
 
 end = time()
