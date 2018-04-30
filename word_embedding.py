5dd5d7d854caabc751f2d1aa16b8d3899ee01981
haswelltitan <545254534@qq.com>
2018-04-30 18:20:58 +0800

    gensim word2vec

--- word_embedding.py
+++ word_embedding.py
@@ -1,41 +1,38 @@
 from time import time
+from gensim.models import word2vec
+import numpy as np
 
 start = time()
-num = 0
 raw = []
-dic = {}
-dic_size = 128
-for buffer in open('train_seg.txt', encoding='utf-8').readlines()[:100]:
-    raw.append(buffer.strip().split())
+num = 0
+size = 128
+for line in open('train_seg.txt', encoding='utf-8').readlines():
+    raw.append(line.strip().split())
     num += 1
-for buffer in open('test_seg.txt', encoding='utf-8').readlines()[:100]:
-    raw.append(buffer.strip().split())
-for line in raw:
-    for word in line:
-        if word in dic:
-            dic[word] += 1
-        else:
-            dic[word] = 1
-top = sorted(dic.keys(), key=dic.__getitem__, reverse=True)[:dic_size-1]
-dic = {}
-for word in top:
-    dic[word] = len(dic) + 1
-clean = []
-for line in raw:
-    temp = []
-    for word in line:
-        if word in dic:
-            temp.append(dic[word])
-        else:
-            temp.append(0)
-    clean.append(temp)
-del raw
+for line in open('test_seg.txt', encoding='utf-8').readlines():
+    raw.append(line.strip().split())
+model = word2vec.Word2Vec(raw, size=size, min_count=10, workers=4)
 
-count = [['UNK', -1]]
-for word in range(len(top)):
-    count.append([top[word], dic[top[word]]])
-print(len(count))
-for line in raw:
-    data = []
+outfile = open('train_vec.txt', 'w')
+for line in raw[:num]:
+    vec = np.zeros(size)
+    for word in line:
+        if word in model:
+            vec = vec + model[word]
+    for word in vec:
+        outfile.write(str(round(word, 4))+' ')
+    outfile.write('\n')
+outfile.close()
+outfile = open('test_vec.txt', 'w')
+for line in raw[num:]:
+    vec = np.zeros(size)
+    for word in line:
+        if word in model:
+            vec = vec + model[word]
+    for word in vec:
+        outfile.write(str(round(word, 4))+' ')
+    outfile.write('\n')
+outfile.close()
+
 end = time()
 print(end-start)
