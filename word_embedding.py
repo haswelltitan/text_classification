2564aa06aee9544b037397b48a9f547bfcd244ce
haswelltitan <545254534@qq.com>
2018-05-01 09:07:53 +0800

    downsizing feature vector

--- word_embedding.py
+++ word_embedding.py
@@ -5,34 +5,34 @@ import numpy as np
 start = time()
 raw = []
 num = 0
-size = 128
+size = 64
 for line in open('train_seg.txt', encoding='utf-8').readlines():
     raw.append(line.strip().split())
     num += 1
 for line in open('test_seg.txt', encoding='utf-8').readlines():
     raw.append(line.strip().split())
 model = word2vec.Word2Vec(raw, size=size, min_count=10, workers=-1)
-model.save('model')
-outfile = open('train_vec.txt', 'w')
-for line in raw[:num]:
-    vec = np.zeros(size)
-    for word in line:
-        if word in model:
-            vec = vec + model[word]
-    for word in vec:
-        outfile.write(str(round(word, 4))+' ')
-    outfile.write('\n')
-outfile.close()
-outfile = open('test_vec.txt', 'w')
-for line in raw[num:]:
-    vec = np.zeros(size)
-    for word in line:
-        if word in model:
-            vec = vec + model[word]
-    for word in vec:
-        outfile.write(str(round(word, 4))+' ')
-    outfile.write('\n')
-outfile.close()
+model.save('model.txt')
+# outfile = open('train_vec.txt', 'w')
+# for line in raw[:num]:
+#     vec = np.zeros(size)
+#     for word in line:
+#         if word in model:
+#             vec = vec + model[word]
+#     for word in vec:
+#         outfile.write(str(round(word, 4))+' ')
+#     outfile.write('\n')
+# outfile.close()
+# outfile = open('test_vec.txt', 'w')
+# for line in raw[num:]:
+#     vec = np.zeros(size)
+#     for word in line:
+#         if word in model:
+#             vec = vec + model[word]
+#     for word in vec:
+#         outfile.write(str(round(word, 4))+' ')
+#     outfile.write('\n')
+# outfile.close()
 
 end = time()
 print(end-start)
